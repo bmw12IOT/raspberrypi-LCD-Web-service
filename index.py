04eb9888353452d792dc7c7c2c85a42424e0447d
Donato Wolfisberg <d.w@hotmail.ch>
2017-01-12 21:40:22 +0100

    Rewritten to python

---
+++ index.py
@@ -0,0 +1,43 @@
+from BaseHTTPServer import BaseHTTPRequestHandler,HTTPServer
+import lcddriver
+from time import *
+import urlparse
+
+PORT_NUMBER = 8080
+lcd = lcddriver.lcd()
+lcd.lcd_clear()
+
+def changeLCDMessage(msg):
+    lcd.lcd_display_string("DonatoPI", 1)
+    lcd.lcd_display_string("", 2)
+    lcd.lcd_display_string(msg, 3)
+    lcd.lcd_display_string("", 4)
+
+class myHandler(BaseHTTPRequestHandler):
+
+	def do_GET(self):
+		self.send_response(200)
+		self.send_header('Content-type','text/json')
+		self.end_headers()
+
+        qs = {}
+        path = self.path
+        if '?' in path:
+            path, tmp = path.split('?', 1)
+            qs = urlparse.parse_qs(tmp)
+            changeLCDMessage(qs["msg"])
+
+		# Send the html message
+		self.wfile.write("Hello World !")
+		return
+
+try:
+
+	server = HTTPServer(('', PORT_NUMBER), myHandler)
+	print 'Started httpserver on port ' , PORT_NUMBER
+
+	server.serve_forever()
+
+except KeyboardInterrupt:
+	print '<taste zum beenden>'
+	server.socket.close()
